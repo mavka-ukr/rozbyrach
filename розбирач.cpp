901b116b64bc71b71347ec76b4ad4bea8c7552f0
Davyd Kohut <davyd.kohut@gmail.com>
2025-02-16 18:55:05 +0200

    init

---
+++ розбирач.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string>
+#include <cstring>
+
+extern "C" size_t read_file(char *path, size_t path_size, char **out) {
+    FILE *file = fopen(strdup(std::string(path, path_size).c_str()), "rb");
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    char *value = (char *) malloc(file_size);
+    fread(value, file_size, 1, file);
+    fclose(file);
+    *out = value;
+    return file_size;
+}
+
+extern "C" void print_long(long value) {
+    printf("%ld", value);
+}
+
+extern "C" void println_long(long value) {
+    printf("%ld\n", value);
+}
